f5f92b4d62a7ac9d6fb054e49447aa358e4bfd6f
Alf Gaida <agaida@siduction.org>
2013-03-22 03:49:24 +0100

    pyygle.py --> pyygle

--- development/buildHelpDb.sh
+++ development/buildHelpDb.sh
@@ -4,6 +4,6 @@ for lang in de en it pl pt-br pt ro ; do
 	DB=/tmp/sidu-manual_$lang.db
 	echo $DB ...
 	if [ ! -f $DB ] ; then
-		python ../../pyygle/pyygle_src/pyygle.py --db=$DB parse fill-db ../website/static/$lang '[.]htm$' | tee -a /tmp/pyygle.sm.log
+		python ../../pyygle/pyygle_src/pyygle --db=$DB parse fill-db ../website/static/$lang '[.]htm$' | tee -a /tmp/pyygle.sm.log
 	fi
 done
