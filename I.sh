6c05c38f77cb949b4465cce3eb840580f9de7e1e
Alf Gaida <agaida@siduction.org>
2013-12-30 22:05:14 +0100

    yeah release, refs #1448

--- I.sh
+++ I.sh
@@ -3,9 +3,9 @@
 sudo chown -R hm /usr/share/sidu-manual /usr/share/sidu-base
 rsync -vua data /usr/share/sidu-manual/data
 cp -v templates/* /usr/share/sidu-manual/templates
-cp -v source/* /usr/share/sidu-manual/source
+cp -v msource/* /usr/share/sidu-manual/msource
 cp -v config/* /usr/share/sidu-manual/config
 #cp -v website/static/lib/flags/* /usr/share/sidu-manual/website/static/flags
-cd ../sidu-base
+cd ../../sidu-base
 rsync -va util/ webbasic/ /usr/share/sidu-base
 
--- import_config.sh
+++ import_config.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-python ../sidu-base/util/configurationbuilder.py -v --summary --drop-tables --prefix=sidu-base data/config.db ../sidu-base/config
-python ../sidu-base/util/configurationbuilder.py -v --summary --prefix=sidu-manual data/config.db config
+python ../../sidu-base/master/util/configurationbuilder.py -v --summary --drop-tables --prefix=sidu-base data/config.db ../../sidu-base/master/config
+python ../../sidu-base/master/util/configurationbuilder.py -v --summary --prefix=sidu-manual data/config.db config
 
