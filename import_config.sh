f7510a5b86321c0ca877976044fb77dc64436069
Axel Konrad <emma-konrad@web.de>
2021-04-24 16:38:37 +0200

    WIP bereinigt Nr.1

--- import_config.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python ../sidu-base/util/configurationbuilder.py -v --summary --drop-tables --prefix=sidu-base data/config.db ../../sidu-base/master/config
-python ../sidu-base/util/configurationbuilder.py -v --summary --prefix=sidu-manual data/config.db config
-
