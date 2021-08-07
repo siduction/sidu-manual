d97eb2bd6dd90e5343911b0a3fb9736004e4a82e
Axel Konrad <emma-konrad@web.de>
2021-08-07 19:53:30 +0200

    issue #19 finished

--- development/menue-extract.pl
+++ development/menue-extract.pl
@@ -20,7 +20,7 @@
 use strict;
 use File::Basename;
 
-my ($FILE, $NR, $EBENE1, $EBENE2, $CLASS, $TEXT, $LINK, $LINK2, $LINK1);
+my ($FILE, $NR, $EBENE1, $EBENE2, $TAB, $TAB2, $CLASS, $TEXT, $ANKER, $LINK, $LINK1);
 my (@DATEIEN, @QUELLE, @LAGER, @MENU_FILE);
 
 ######## HTML Anfang erzeugen
@@ -70,7 +70,6 @@ while (@DATEIEN) {
 ######## Beginn Überschriften auswerten.
     START: while (@LAGER) {
         $_ = shift @LAGER;
-        chomp($_);
 
         if (/^# /) {
             if ($NR > 1) {
@@ -78,27 +77,36 @@ while (@DATEIEN) {
             }    
             $EBENE1 ++;
             $EBENE2 = 0;
+            $TAB = "\t\t";
+            chomp($_);
             &LINK_LINE;
-            push @MENU_FILE, "\t<li class=\"versteckt\">\n\t\t<input type=\"checkbox\" id=\"M$EBENE1\"/>\n\t\t<label for=\"M$EBENE1\">$LINK1</label>\n\t\t<ul>\n";
+            push @MENU_FILE, "\t<li class=\"versteckt\">\n$TAB<input type=\"checkbox\" id=\"M$EBENE1\"/>\n$TAB<label for=\"M$EBENE1\">$TEXT</label>\n$TAB<ul>\n\t$TAB<li>$LINK1</li>\n";
             next;
-        } else {
+        } elsif (/^## /) {
             $EBENE2 ++;
+            $TAB = "\t\t\t\t";
+            chomp($_);
             &LINK_LINE;
-                push @MENU_FILE, "\t\t\t<li class=\"versteckt\">\n\t\t\t\t<input type=\"checkbox\" id=\"M$EBENE1" . "_" . "$EBENE2\"/>\n\t\t\t\t<label for=\"M$EBENE1" . "_" . "$EBENE2\">$LINK2</label>\n\t\t\t\t<ul>\n";
+                push @MENU_FILE, "\t\t\t<li class=\"versteckt\">\n$TAB<input type=\"checkbox\" id=\"M$EBENE1" . "_" . "$EBENE2\"/>\n$TAB<label for=\"M$EBENE1" . "_" . "$EBENE2\">$TEXT</label>\n$TAB<ul>\n$TAB\t<li>$LINK1</li>\n";
+                next;
+        } else {
+            unshift @LAGER, $_;
             while (@LAGER) {
                 $_ = shift @LAGER;
-                if (/^### /) {
-                    chomp($_);
-                    &LINK_LINE;
-                    $LINK = "\t\t\t\t\t<li>$LINK</li>\n";
-                    push @MENU_FILE, "$LINK";
-                } else {
-                    push @MENU_FILE, "\t\t\t\t</ul>\n\t\t\t</li>\n";
+                if (/^## /) {
+                    push @MENU_FILE, "\t$TAB</ul>\n$TAB</li>\n";
                     unshift @LAGER, $_;
                     next START;
+                } else {
+                    chomp($_);
+                    &LINK_LINE;
+                    push @MENU_FILE, "$TAB\t<li>$LINK</li>\n";
                 }
             }
-            push @MENU_FILE, "\t\t\t\t</ul>\n\t\t\t</li>\n";
+            if ( $TAB eq "\t\t\t\t") {
+                $TAB2 = "\t\t\t";
+                push @MENU_FILE, "$TAB</ul>\n$TAB2</li>\n";
+            }
         }
     }
 }
@@ -108,26 +116,21 @@ push @MENU_FILE, "\t\t</ul>\n\t</li>\n</ul>\n</body>\n</html>\n";
 sub LINK_LINE {
 
         # Formatfehler (Leerzeichen am Zeilenende) beseitigen.
-        print "$_\n";
-        $_ =~ s!^(#{1,4}.*)\s+$!$1!;
+        $_ =~ s!^(#{1,3}.*)\s+$!$1!;
         
         # Zeilen zerlegen und neu zusammensetzen.
         $CLASS = $_;
         $CLASS =~ s/^(#{1,3}) .*/$1/;
 
         $TEXT = $_;
-        $TEXT =~ s/^#{1,3} (.*)/$1/;
+        $TEXT =~ s/^#{1,3} +(.*)/$1/;
         
-        $LINK = "$FILE\#$TEXT";
-        $LINK =~ s!(.*)!\L$1!;
-        $LINK =~ s!( )!-!g;
-        $LINK = "<a target= \"main\" href=\"$LINK\">$TEXT</a>";
+        $ANKER = $TEXT;
+        $ANKER =~ s!(.*)!\L$1!;
+        $ANKER =~ s!( )!-!g;
+        
+        $LINK = "<a target= \"main\" href=\"$FILE\#$ANKER\">$TEXT</a>";
         $LINK1 = "<a target= \"main\" href=\"$FILE\">$TEXT</a>";
-        
-        $LINK2 = "$FILE";
-        $LINK2 =~ s!(.*)!\L$1!;
-        $LINK2 =~ s!( )!-!g;
-        $LINK2 = "<a target= \"main\" href=\"$LINK2\">$TEXT</a>";
 }
 
 ######## In Ausgabedateien schreiben.
